bce71ee60b80103cf1a33d6c26d34716ce4d3df2
LagzaniaCat <grace.l.mackenzie@icloud.com>
2026-01-28 19:37:20 -0700

    Added Elipse class

---
+++ kernel/app/src/main/java/com/example/kernel/Elipse.java
@@ -0,0 +1,13 @@
+/**
+ * A representative class for an Elipse. Extends Shape.
+ */
+class Elipse extends Shape {
+    // Atributes
+    private String color;
+
+    // Constructors
+    public Elipse (int x, int y) {
+        super(x, y);
+        this.color = "purple";
+    }
+}
