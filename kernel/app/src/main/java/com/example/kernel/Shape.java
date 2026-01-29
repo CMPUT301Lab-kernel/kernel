ed27522ecbf735d19fdd587213bcf10c5b520929
rsheikhdon <rsheikhdon124@gmail.com>
2026-01-28 18:19:51 -0700

    Update Shape.java

--- kernel/app/src/main/java/com/example/kernel/Shape.java
+++ kernel/app/src/main/java/com/example/kernel/Shape.java
@@ -3,4 +3,12 @@ package com.example.kernel;
 public abstract class Shape {
     int x;
     int y;
+    String color;
+
+    public class Shape(int x int y){
+        this.x = x;
+        this.y = y;
+        color = "blue";
+    }
 }
+
